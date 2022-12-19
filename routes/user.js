5ddf66b8d7649d88db16ef08c5360b79a34667b2
Mohd Zeeshan <100269606+MohdZeeshan11@users.noreply.github.com>
2022-12-20 05:17:47 +0530

    file added

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,6 @@ const express = require('express')
 const router = express.Router()
 
 const {
-    // getUser, 
-    // addUser,
     userLogin,
     userRegister,
     getPlayer,
@@ -16,17 +14,15 @@ const {
 
 } = require('../controller/user')
 const validateToken = require('../middleware/authHandler')
-
-// router.route('/card/details').get(getUser).post(validateToken,addUser)
 router.route('/login').post(userLogin)
 router.route('/index-value/:playerName').get(validateToken,getIndexValue)
 router.route('/index-value').post(validateToken,setIndexValue)
 router.route('/get-player').post(validateToken,getPlayer)
 router.route('/register').post(userRegister)
-router.route('/card').post(userCardCreate)
-router.route('/card/details').get(getUserCardDetails)
-router.route('/card/winner').post(saveWinnerName)
-router.route('/card/winnerValue').post(saveWinnerValue)
-// router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser)
+router.route('/card').post(validateToken,userCardCreate)
+router.route('/card/details').get(validateToken,getUserCardDetails)
+router.route('/card/winner').post(validateToken,saveWinnerName)
+router.route('/card/winnerValue').post(validateToken,saveWinnerValue)
+
 
 module.exports = router
